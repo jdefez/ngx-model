e1de06d038558f6b57b13ae559e342467daed50d
jean <jdefez@gmail.com>
2018-04-02 21:57:21 +0200

    package update

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var model_1 = require("../src/model");
+var model_1 = require("./lib/model");
 exports.Model = model_1.Model;
