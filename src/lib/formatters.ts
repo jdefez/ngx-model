a4f1b777fad8fb80c723080dd213b5742896189c
jean <jdefez@gmail.com>
2018-05-24 16:10:34 +0200

    Only Format if value is defined

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,24 +1,30 @@
 export class Formatters {
   static toInteger(value: any): number {
-    value = String(value).replace(/\s+/, '');
-    if (Number.isNaN(parseInt(value, 10))) {
-      return 0;
-    } else {
-      return parseInt(value, 10);
+    if (value) {
+      value = String(value).replace(/\s+/, '');
+      if (Number.isNaN(parseInt(value, 10))) {
+        return 0;
+      } else {
+        return parseInt(value, 10);
+      }
     }
   }
 
   static toFloat(value: any): number {
-    value = String(value).replace(/,/, '.');
-    if (Number.isNaN(Number.parseFloat(value))) {
-      return 0;
-    } else {
-      return Number.parseFloat(value);
+    if (value) {
+      value = String(value).replace(/,/, '.');
+      if (Number.isNaN(Number.parseFloat(value))) {
+        return 0;
+      } else {
+        return Number.parseFloat(value);
+      }
     }
   }
 
   static toString(value: any): string {
-    return String(value);
+    if (value) {
+      return String(value);
+    }
   }
 
   static toBoolean(value: any): boolean {
