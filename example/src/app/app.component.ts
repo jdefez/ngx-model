f0101f873cd63c81b9815cef16893ffbd733860d
jean <jdefez@gmail.com>
2018-09-11 18:09:11 +0200

    dumpTest cleaned

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { TagExtra } from './models/tag-extra';
 import { User } from './models/user';
 import { MyUser } from './models-my/my-user';
 import { Tag } from './models/tag';
+import * as  USER_JSON from './my-user.json';
 
 @Component({
   selector: 'app-root',
@@ -31,77 +32,7 @@ export class AppComponent implements OnInit {
   }
 
   dumpTest() {
-    this.myUser = new MyUser({
-      "id":30841,
-      "first_name":"",
-      "last_name":"",
-      "email":"nicolasvv@previsite.com",
-      "source_id":"1",
-      "source_ref":"509E70CF-56A6-48AE-6024-0E8EA68BF409",
-      "created_at":null,
-      "updated_at":"2018-07-16 13:45:47",
-      "deleted_at":null,
-      "customer_id":null,
-      "language":"FR",
-      "locale":"FR_FR",
-      "logo":null,
-      "active_listings":8,
-      "listing_quota":0,
-      "profile_progress":1,
-      "products":[{
-        "ref":"spot_video","visible":false,"subscribed":false,"parent":"","total":0,"limit":null,
-        "options":[{
-          "name":"allow_download","title":"allow_download","possible_values":null,"type":"bool","value":false
-        }]
-      },{
-        "ref":"social_connect","visible":false,"subscribed":false,"parent":"","limit":0,"total":0,
-        "options":[{
-          "name":"allow_disconnect","title":"allow_disconnect","possible_values":null,"type":"bool","value":true}
-        ],
-        "portals":[{
-          "ref":"youtube","parent":"social_connect","subscribed":false,"limit":0,"total":0},{"ref":"facebook","parent":"social_connect","subscribed":false,"limit":0,"total":0},{"ref":"twitter","parent":"social_connect","subscribed":false,"limit":0,"total":0},{"ref":"dailymotion","parent":"social_connect","subscribed":false,"limit":0,"total":0},{"ref":"facebookPage","parent":"social_connect","subscribed":false,"limit":0,"total":0
-          }]
-      },{
-        "ref":"sites_mandats","visible":false,"subscribed":false,"parent":"","limit":0,"total":0
-      },{
-        "ref":"virtual_visit","visible":true,"subscribed":true,"parent":"","total":"8","limit":0
-      },{
-        "ref":"livevisit","visible":false,"subscribed":true,"parent":"virtual_visit","total":null,"limit":null
-      }],
-      "property_types":[{
-        "id":"0","name":"Autre"
-      },{
-        "id":"1","name":"Appartement"
-      },{"id":"2","name":"Maison"
-      },{
-        "id":"3","name":"Terrain"
-      },{
-        "id":"4","name":"Bureau"
-      },{
-        "id":"5","name":"Immeuble"
-      },{
-        "id":"6","name":"Loft"
-      },{
-        "id":"9","name":"Commerce"
-      },{
-        "id":"11","name":"Parking"
-      },{
-        "id":"12","name":"Ferme"
-      },{
-        "id":"102","name":"Manoir"
-      }],
-      "agency": {
-        "id":"915B6C9C-D077-77FD-81E4-5E693F341E98",
-        "name":"Test nico vv",
-        "website":"http:\/\/www.previsite.fr\/",
-        "email":"",
-        "address":"15 rue du bout du monde",
-        "city":"Paris",
-        "country":"FR",
-        "zip_code":"14001",
-        "logo":"https:\/\/media.previsite.com\/agency\/F2B47E58-4978-C8A7-7976-F764BC041B50.png"
-      }
-    });
+    this.myUser = new MyUser(USER_JSON);
   }
 
   loadUsers() {
